4ebbded941e421b224487f7ab8f459aa8b3d0385
Kaito-Dogi <kendobu0405@gmail.com>
2022-10-29 08:57:05 +0900

    feat: アニメーションの実装

--- app/src/main/java/app/doggy/objectanimatorsample/MainActivity.kt
+++ app/src/main/java/app/doggy/objectanimatorsample/MainActivity.kt
@@ -1,16 +1,37 @@
 package app.doggy.objectanimatorsample
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
+import android.view.View
+import android.view.animation.DecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import app.doggy.objectanimatorsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+  private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    binding.circle.setOnClickListener {
+      it.startMoveToPointAnim(
+        transX = 500f,
+        transY = 500f,
+      )
     }
+  }
+
+  private fun View.startMoveToPointAnim(transX: Float, transY: Float) {
+    val translationX = PropertyValuesHolder.ofFloat(View.TRANSLATION_X, transX)
+    val translationY = PropertyValuesHolder.ofFloat(View.TRANSLATION_Y, transY)
+    val animator = ObjectAnimator.ofPropertyValuesHolder(this, translationX, translationY).apply {
+      duration = 1000
+      interpolator = DecelerateInterpolator() // 急速に開始し、その後減速しながら変化させる
+    }
+    animator.start()
+  }
 }
